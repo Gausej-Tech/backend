c77210a9dfb4a86b57afdaf0256761815524ce2a
Satish Singh <i.sksingh113@gmail.com>
2025-06-27 12:28:04 +0530

    update

--- server.js
+++ server.js
@@ -6,15 +6,27 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 7891;
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.CLIENT_URL_1,
+  process.env.CLIENT_URL_2,
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+  
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS: " + origin));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
